49d2e626aea13b084cd9ebb39db5ba92daf71c8c
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2022-03-08 19:23:10 +0700

    feat: pwa configuration

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,16 @@
+import React from "react";
 import {
   Links,
-  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLocation,
+  useMatches,
 } from "remix";
+import type { MetaFunction, LinksFunction } from "remix";
 import TailwindStyles from "./styles/tailwind.css";
-import type { MetaFunction } from "remix";
 import { BaseLayout } from "./components/Layout";
 
 export const meta: MetaFunction = () => {
@@ -20,21 +22,56 @@ export const links: LinksFunction = () => {
 };
 
 export default function App() {
+  let location = useLocation();
+  let matches = useMatches();
+
+  let isMount = true;
+  React.useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <Meta /> <Links />
       </head>
       <body className="relative flex flex-col items-center bg-gray-700 text-white">
         <BaseLayout>
           <Outlet />
         </BaseLayout>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <ScrollRestoration /> <Scripts /> <LiveReload />
       </body>
     </html>
   );
